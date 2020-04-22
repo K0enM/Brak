0b8d5df066f9560b487c330df62c3cc7c79ad498
Koen Molenaar <k.molenaar@student.utwente.nl>
2020-04-23 01:49:09 +0200

    Create view begin

--- BrakSite/BrakApp/views.py
+++ BrakSite/BrakApp/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'BrakApp/index.html')
+
+
+def error(request):
+    return render(request, '404.html')
+
+
+def create(request):
+    return render(request, 'BrakApp/create.html')
